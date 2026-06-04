e585b439136a4fea25928ccc5ce9913456c7a879
Srisha Radhakrishnan <srisharadhakrishnan2006@gmail.com>
2026-06-04 20:04:49 +0530

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a plant shopping application that provides a wide
+        variety of indoor and outdoor plants for plant lovers.
+      </p>
+
+      <p>
+        Our mission is to bring nature closer to every home with beautiful and
+        affordable plants.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
