f53feb26bcc4d3261fb214af9eca269edd84c12e
Srisha Radhakrishnan <srisharadhakrishnan2006@gmail.com>
2026-06-04 16:15:27 +0530

    Create CartItem.jsx

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,11 @@
+<button onClick={() => dispatch(increaseQuantity(id))}>
++
+</button>
+
+<button onClick={() => dispatch(decreaseQuantity(id))}>
+-
+</button>
+
+<button onClick={() => dispatch(removeItem(id))}>
+Delete
+</button>
