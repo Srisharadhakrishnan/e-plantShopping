47dbc99553834702ec0f00ad74fdbf41e5a2e6e0
Srisha Radhakrishnan <srisharadhakrishnan2006@gmail.com>
2026-06-04 16:18:28 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
